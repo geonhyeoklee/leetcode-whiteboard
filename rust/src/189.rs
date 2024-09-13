10ddcd503eef9d321a0436d378502d26013428ad
GeonHyeokLee <geonhyeoklee.kr@gmail.com>
2024-09-13 14:38:20 +0900

    feat: add rust solution for 189

--- rust/src/189.rs
+++ rust/src/189.rs
@@ -1,14 +1,17 @@
 struct Solution;
+
 impl Solution {
     pub fn rotate(nums: &mut Vec<i32>, k: i32) {
-        let n = nums.len();
-        let k = k as usize % n;
-
-        nums.reverse();
-        nums[..k].reverse();
-        nums[k..].reverse();
+        for _ in 0..k {
+            if let Some(num) = nums.pop() {
+                nums.reverse();
+                nums.push(num);
+                nums.reverse();
+            }
+        }
     }
 }
+
 fn main() {
     let mut nums = vec![1, 2, 3];
     let k = 4;
